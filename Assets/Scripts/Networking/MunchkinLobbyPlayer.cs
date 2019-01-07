d7bc6c1a0e6fd0744a48a73857fe48458f5c2989
Ryan Comer <ryancomer94@gmail.com>
2019-01-07 01:09:39 -0600

    Started working on multiplayer

---
+++ Assets/Scripts/Networking/MunchkinLobbyPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MunchkinLobbyPlayer : NetworkLobbyPlayer
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

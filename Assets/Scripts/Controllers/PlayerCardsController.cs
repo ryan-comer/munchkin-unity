427be5c4acd2ea8da00bba52a9fe0bfd3ccb204b
Ryan Comer <ryancomer94@gmail.com>
2019-01-16 00:14:26 -0600

    UI is set up for the player cards.  Still working on controller to add funcitonality

---
+++ Assets/Scripts/Controllers/PlayerCardsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerCardsController : NetworkBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
